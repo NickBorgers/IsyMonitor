d34b8579a9bac3153a096569df22360c4034eeb4
NickBorgers <nickborgers@gmail.com>
2019-01-20 11:10:58 -0600

    need to add def file...

---
+++ EventDispositions.py
@@ -0,0 +1,35 @@
+ignoredEventTypes = [
+  "Lux",
+  "Ultraviolet Light Index",
+  "Climate",
+  "Billing",
+  "Status",
+  "Something Sonos 1",
+  "Something Sonos 2",
+  "System Config Updated",
+  "Wind Direction",
+  "System Status",
+  "Fan Mode Overide",
+  "Nodes Updated",
+  "System Progress"
+]
+
+triggerTypeEvents = [
+  "Trigger",
+  "Device On",
+  "Device Off",
+  "Error"
+]
+statusTypeEvents = [
+  "Battery Level",
+  "Thermostat Reading",
+  "Humidity",
+  "Thermostat Mode",
+  "Nanoleaf Aurora Brightness",
+  "Nanoleaf Aurora Scene Number",
+  "Sonos Speaker Volume",
+  "User Number",
+  "Heat/Cool State",
+  "Fan State",
+  "Heat Setpoint"
+]
