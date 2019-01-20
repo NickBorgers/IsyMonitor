fc38a58469aadb731d3799a1d696af77380fef83
NickBorgers <nickborgers@gmail.com>
2019-01-20 11:28:03 -0600

    flatter class

---
+++ ProgramStatusAnalysis.py
@@ -0,0 +1,10 @@
+  @staticmethod
+  def condition_status(conditionIndicator):
+    if conditionIndicator is 1:
+      return 'unknown'
+    elif conditionIndicator is 2:
+      return 'true'
+    elif conditionIndicator is 3:
+      return 'false'
+    else :
+      return 'not_loaded'
