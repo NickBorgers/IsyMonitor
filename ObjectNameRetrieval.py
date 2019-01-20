e3fe17715cb30254bf4e796758f6a86f8c2e2487
NickBorgers <nickborgers@gmail.com>
2019-01-20 11:39:30 -0600

    forgot to add isy to parameters

--- ObjectNameRetrieval.py
+++ ObjectNameRetrieval.py
@@ -1,5 +1,5 @@
 
-def get_program_name(event):
+def get_program_name(event, myisy):
   programId = event.find("eventInfo").find("id").text
   paddedProgramId = programId.rjust(4, '0')
   return myisy.get_prog(paddedProgramId).name
