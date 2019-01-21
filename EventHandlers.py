2878793b6c1acf04e21df582de23b0d6d5ad09c9
NickBorgers <nickborgers@gmail.com>
2019-01-21 08:50:54 -0600

    try changing import form

--- EventHandlers.py
+++ EventHandlers.py
@@ -1,7 +1,7 @@
 import EventDispositions
 import ProgramStatusAnalysis
 import ObjectNameRetrieval
-import Logger
+import Logger from Logger
 
 def handleProgramEvent(logger, event, myisy):
   statusIndicator = event.find("eventInfo").find("s").text
