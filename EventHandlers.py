786c094dd4859ca2bec650ee2794bdb55b5e498b
NickBorgers <nickborgers@gmail.com>
2019-01-22 17:55:47 -0600

    add messages to logs

--- EventHandlers.py
+++ EventHandlers.py
@@ -14,29 +14,34 @@ def handleProgramEvent(logger, event, myisy):
     pass
   else:
     programName = ObjectNameRetrieval.get_program_name(event, myisy)
+    message = "Program (" + programName + ") is (" + statusIndicator + ") " + programStatus + " with condition " + conditionStatus
     logObject = {
       "type": "program",
       "object_name": programName,
       "new_status" : programStatus,
       "condition": conditionStatus,
-      "status_detail": statusIndicator
+      "status_detail": statusIndicator,
+      "message": message
     }
     logger.logThis(logObject)
-    print ("Program (" + programName + ") is (" + statusIndicator + ") " + programStatus + " with condition " + conditionStatus)
+    print (message)
     
     
 def handleVariableChange(logger, event, myisy):
   variableName = ObjectNameRetrieval.get_variable_name(event, myisy)
       
   variableNewValue = event.find("eventInfo").find("var").find("val").text
-    
+  
+  message = "Variable (" + variableName + ") is now: " + variableNewValue
+  
   logObject = {
     "type": "variable",
     "object_name": variableName,
-    "new_status" : variableNewValue,
+    "new_status": variableNewValue,
+    "message": message
   }
   logger.logThis(logObject)
-  print ("Variable (" + variableName + ") is now: " + variableNewValue)
+  print (message)
   
 def handleTriggerEvent(logger, event, control, control_action, nodename):
   try :
@@ -47,14 +52,16 @@ def handleTriggerEvent(logger, event, control, control_action, nodename):
   if control not in EventDispositions.ignoredEventTypes :
     if control_action is not None:
       if control_action != 'Info String' :
+        message = control + " by: " + nodename + " : " + control_action
         logObject = {
           "type": "trigger",
           "object_name": nodename,
           "new_status" : control,
-          "status_detail": control_action
+          "status_detail": control_action,
+          "message": message
         }
         logger.logThis(logObject)
-        print (control + " by: " + nodename + " : " + control_action)
+        print (message)
     else:
       logObject = {
         "type": "trigger",
@@ -67,28 +74,34 @@ def handleTriggerEvent(logger, event, control, control_action, nodename):
 def handleStatusEvent(logger, event, control, nodename):
   if nodename is not None :
     statusDetail = event.find("fmtAct").text
+    message = "Status (" + control + ") of: " + nodename + " is: " + statusDetail
     logObject = {
       "type": "status",
       "object_name": nodename,
       "attribute" : control,
-      "status_detail": statusDetail
+      "status_detail": statusDetail,
+      "message": message
     }
     logger.logThis(logObject)
-    print ("Status (" + control + ") of: " + nodename + " is: " + statusDetail)
+    print (message)
   else :
+    message = "Status (no nodename could be determined)"
     logObject = {
       "type": "status",
       "object_name": "unknown",
-      "new_status" : control
+      "new_status": control,
+      "message": message
     }
     logger.logThis(logObject)
-    print ("Status (no nodename could be determined)")
+    print (message)
     
 def handleOtherNodeEvent(logger, control, node_address, nodename):
+  message = "Other event type for known node: " + control + " : " + nodeaddress + ": " + nodename
   logObject = {
     "type": "other",
     "object_name": nodeaddress,
-    "new_status" : control
+    "new_status": control,
+    "message": message
   }
   logger.logThis(logObject)
-  print ("Other event type for known node: " + control + " : " + nodeaddress + ": " + nodename)
+  print (message)
