e894dd50451aa2c1c9764392ab696bedf9e1c65e
NickBorgers <nickborgers@gmail.com>
2019-02-02 12:22:15 -0600

    make path based name optional

--- ObjectNameRetrieval.py
+++ ObjectNameRetrieval.py
@@ -2,13 +2,19 @@
 def get_program_name(event, myisy):
   programId = event.find("eventInfo").find("id").text
   paddedProgramId = programId.rjust(4, '0')
-  return myisy.prog_get_path(paddedProgramId)
+  try:
+    return myisy.prog_get_path(paddedProgramId)
+  except: 
+    return myisy.get_prog(paddedProgramId).name
   
 def get_node_name(event, myisy):
   nodeaddress = event.find('node').text
   if nodeaddress is None:
     nodeaddress = event.find("eventInfo").text[1:13].strip()
-  return myisy.node_get_path(nodeaddress)
+  try:
+    return myisy.node_get_path(nodeaddress)
+  except: 
+    return myisy._node_get_name(nodeaddress)[1]
   
 def get_detailed_control(event):
   eventControlText = event.find("eventInfo").text[14:23].strip()
